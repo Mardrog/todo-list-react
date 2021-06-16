af984a567cdcc392eb18ff386e3115cadbafada6
Pawel Wojnowiak <pawel.wojnowiak@gmail.com>
2021-06-16 22:02:00 +0200

    changed task status on "false" in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./Header";
 
 const tasks = [
   { id: 1, content: "Zabrać się za zadanie z tygodnia 8", done: true },
-  { id: 2, content: "Odrobić zadanie z tygodnia 8", done: true },
+  { id: 2, content: "Odrobić zadanie z tygodnia 8", done: false },
 ];
 
 const hideDoneTasks = false;
